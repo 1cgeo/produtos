1fe01687fa93a5e48148ce3da5de867dbddc271c
JaimeGuilherme <jaime.guilherme67@gmail.com>
2024-09-09 13:10:14 -0300

    Remove button

--- js/main.js
+++ js/main.js
@@ -268,7 +268,7 @@ moveBySearch = (val, searchEl) => {
         errorEl.id = 'error-message';
         errorEl.style.color = 'red';
         errorEl.style.margin = '5px';
-        errorEl.textContent = 'Nenhuma feature encontrada.';
+        errorEl.textContent = 'Nenhum produto encontrado.';
         searchEl.appendChild(errorEl);
     }
 };
@@ -285,23 +285,19 @@ loadSearch = (searchElId) => {
 
     let input = `
         <div style="display: flex; align-items: center; margin: 5px;">
-            <span style="margin-right: 5px;">MI:</span>
             <input id="input" type="text" style="flex: 1; padding: 5px;" placeholder="MI/INOM"/>
-            <button id="searchBtn" style="cursor: pointer; margin-left: 5px; padding: 5px;">🔍</button>
         </div>
     `;
 
     searchEl.innerHTML = input;
 
     let inputField = document.getElementById('input');
-    let searchBtn = document.getElementById('searchBtn');
 
     const executeSearch = () => {
         let value = inputField.value;
         moveBySearch(value, searchEl);
     };
 
-    searchBtn.addEventListener('click', executeSearch);
     inputField.addEventListener('keydown', function(event) {
         if (event.key === 'Enter') {
             executeSearch();
